736b70485da4505553caa23ed1d4d390d507c8b7
Yrobot <Yrobot@qq.com>
2024-10-08 23:04:57 +0800

    fix force graph page build

--- src/app/force-graph/child-map/page.tsx
+++ src/app/force-graph/child-map/page.tsx
@@ -1,4 +1,10 @@
-import View from "./View";
+import dynamic from "next/dynamic";
+// import View from "./View";
+
+const View = dynamic(() => import("./View"), {
+  ssr: false,
+  loading: () => <p>Loading Dynamic Component</p>,
+});
 
 export const metadata = {
   title: "Force Graph 3D",
